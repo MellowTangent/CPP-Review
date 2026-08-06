c800ee2b5202217c5e425f6c6a88d8643b17cb18
WildTangent <hector12ico@gmail.com>
2026-08-06 05:52:22 -0700

    removed commented out code for readability

--- cpp-files/chp01/p1.02.cpp
+++ cpp-files/chp01/p1.02.cpp
@@ -1,15 +1,14 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//
-//int main()
-//{
-//	cout << "Hello, my name is Hal!" << endl;
-//	cout << "What would you like me to do?" << endl;
-//	string user_input;
-//	getline(cin, user_input);
-//	cout << "I'm sorry I cannot do that.";
-//
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello, my name is Hal!" << endl;
+	cout << "What would you like me to do?" << endl;
+	string user_input;
+	getline(cin, user_input);
+	cout << "I'm sorry I cannot do that.";
+	return 0;
+}
