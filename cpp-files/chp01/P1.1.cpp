076ccad830597b29e6af3739b05cb20852ca89e1
WildTangent <hector12ico@gmail.com>
2026-08-06 05:40:35 -0700

    changed file name convetion to be consistent and Linux friendly

--- cpp-files/chp01/P1.1.cpp
+++
@@ -1,9 +0,0 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	cout << "Hola mi nombre es Hector Rico!"<< endl;
-//
-//	return 0;
-//}
---
+++ cpp-files/chp01/p1.01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hola mi nombre es HR!"<< endl;
+
+	return 0;
+}
