af04536ddfcd07767096e5ddff2678596dc7c691
WildTangent <hector12ico@gmail.com>
2026-08-07 04:00:40 -0700

    testing code

--- cpp-files/chp04/chp04-test-code.cpp
+++ cpp-files/chp04/chp04-test-code.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-	int year = 0;
-	double balance = 1000;
-	const double target = 3000;
-	const double rate = 5;
-	
 
-	while (balance < target )
-	{
-		year++;
-		double interest = balance * rate / 100;
-		balance = balance + interest;
-	}
-	cout << "Balance: $" << balance << endl;
-	cout << "The investment tripled after " << year << " years" << endl;
+int count = 0;
+srand(time(0));
 
+while (count < 5)
+{
+	int num = rand();
+	cout << "Random number: " << num << endl;
+	count ++;
+}
 	return 0;
 }
