70d5edc503ec442190774df10adb0d5a0c14bdec
WildTangent <hector12ico@gmail.com>
2026-08-11 16:45:27 -0700

    commented test-code for clarity

--- cpp-files/chp04/chp04-test-code.cpp
+++ cpp-files/chp04/chp04-test-code.cpp
@@ -7,13 +7,13 @@ int main()
 {
 
 int count = 0;
-srand(time(0));
+srand(time(0)); // seed the random number generator with the current time
 
-while (count < 5)
+while (count < 5) // loop until 5 random numbers have been generated
 {
-	int num = rand();
-	cout << "Random number: " << num << endl;
-	count ++;
+	int num = rand(); // generate a random number
+	cout << "Random number: " << num << endl; // print the random number
+	count ++; // increment the count
 }
 	return 0;
 }
