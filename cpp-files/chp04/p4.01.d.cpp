3e91bde6984b05c81686c26e90a05ccf0b2b800f
WildTangent <hector12ico@gmail.com>
2026-08-11 01:46:37 -0700

    final version of problem 1 part d, was tricky

--- cpp-files/chp04/p4.01.d.cpp
+++ cpp-files/chp04/p4.01.d.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main () 
 {
-    double a = 0;
-    double b = 0;
+    int a = 0;
+    int b = 0;
+    int sum = 0;
     
     cout << "Enter the first number: ";
     cin >> a;
     cout << endl;
 
+    bool even = a % 2; // check if "a" is even or odd
+
     cout << "Enter the second number: ";
     cin >> b;
     cout << endl;
-    
-    do
+
+    if (even == 0) // if "a" is even we add 1 to it and add it to the accumulator "sum"
     {
-        double odd = 2*a + 1;
-        cout << odd << endl;
-        a++;
+        a = a + 1;
+        sum += a;
+//        cout << sum << endl; // for debugging
+        while (a + 2 <= b) // "a" is now odd so we continously add 2 until we reach upper bound "b"
+        {
+            a = a + 2; // computes next odd number
+            sum += a; // accumulator for when "a" is odd 
+//            cout << a << endl; // for debugging
+        }
     }
-    while (a < b);
+    else // if "a" is odd we skip the body of the if statement and continue here
+    {
+        sum += a; // "a" is already odd so we add it to the accumulator
+//        cout << sum << endl; // for debugging
+        while (a + 2 <= b)
+        {
+            a = a + 2; // computes next odd number
+            sum += a; // accumulator for when "a" is odd
+//            cout << a << endl; // for debugging
+        }
+    }
+    cout << "The sum of all odd nunbers for the given interval is: " << sum << endl;
     return 0;
 }
