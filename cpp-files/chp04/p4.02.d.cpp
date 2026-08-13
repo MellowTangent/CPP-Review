7d069f1229cfc51c70149f036f952227593278f5
WildTangent <hector12ico@gmail.com>
2026-08-12 17:15:24 -0700

    finished p4 part d

---
+++ cpp-files/chp04/p4.02.d.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num = 0; // holds current user input
+    int prev_num = 0; // holds the previous number being compared
+
+
+    cout << "Enter a sequence of numbers to see if they repeat: ";
+
+    cin >> num; // seed num with first value
+    prev_num = num; // set prev_num to first value
+
+    while ( cin >> num ) // while there is still input
+    {
+        if (prev_num == num) // if the previous number is equal to the current number
+        {
+            cout << "The numbers repeat: " << num << endl;
+            prev_num = num; // set previous number to current number
+
+        }
+        else // if the previous number is not equal to the current number
+        {
+            prev_num = num; // set previous number to current number
+        }
+    }
+
+    return 0;
+}
