d2131359ffadf196d6a1646ac1f03e269086ad5c
WildTangent <hector12ico@gmail.com>
2026-08-14 00:27:47 -0700

    fixed/optimized variable layout/scope

--- cpp-files/chp04/p4.03.d.cpp
+++ cpp-files/chp04/p4.03.d.cpp
@@ -5,21 +5,19 @@ using namespace std;
 
 int main ()
 {
-
-    cout << "This progrma will give you the number of vowel in a string of text." << endl;
+    cout << "This progrma will give you the number of vowels in a string of text." << endl;
     string user_input; // declare a string variable to hold the user input
     cout << "Enter a string to parse: "; // prompt user for input
     getline(cin, user_input); // get input from cin and store it in user_input, default delimiter is newline
 
     int string_l = user_input.length(); // get the length of the string and store it in string_l
-    char ch; // declare a char variable to hold each character in the string
     int i = 0; // initialize counter to 0 to iterate through the string
     int vowel_count = 0; // initialize a counter to keep track of the number of vowels
     
     while (i < string_l)
     {
-        ch = user_input[i]; // access each character in the string using the counter variable i and store it in ch
-        char lower_ch = tolower(ch); // convert the character to lowercase to check for vowels
+        char ch = user_input[i]; // initialize a char variable to hold each character in the string
+        char lower_ch = tolower(ch); // initialize a char variable to hold the lowercase version of "char ch" to check for vowels
         if (lower_ch == 'a' || lower_ch == 'e' || lower_ch == 'i' || lower_ch == 'o' || lower_ch == 'u')
         {
             vowel_count++; // increment the vowel counter if a vowel is found
